5eb8fee1eb8f880ae4ff39d93c19312990ddeec1
Robert Bunch <robert.bunch@tinroofsoftware.com>
2015-12-01 09:14:17 -0500

    Init commit of angular map with custom directive

---
+++ js/controller.js
@@ -0,0 +1,40 @@
+var stateMap = angular.module('interactiveMap',[]);
+
+stateMap.controller('interactiveMapCntrl',interactiveMapCntrl);
+
+stateMap.directive('clickState', function(){
+	return{
+		link: function($scope, element){
+
+			element.bind('click', function(){
+				var newColor = getNewColor($scope.state);
+				var stateElement = element[0].querySelector('path');
+				stateElement.setAttribute('class','state ' + newColor);
+			});
+		}
+	}
+});
+
+function interactiveMapCntrl($scope){
+	$scope.states = states;
+}
+
+function getNewColor(state){
+	if(state.stateColor == 'red'){
+		return 'blue';
+	}else if(state.stateColor == 'blue'){
+		return 'open';
+	}else if(state.stateColor == 'open'){
+		return "red";
+	}else{
+		return console.log("error");
+	}
+	//if state color = red, then change to blue
+	//if state color = blue , then change to open
+	//if state state color = open, then change to red
+}
+
+
+
+
+
