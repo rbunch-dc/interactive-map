2770589b0b17db6db74d745b7ae0edea9a9155e1
Robert Bunch <rob@digitalcrafts.com>
2015-12-10 17:14:54 -0500

    Add sag for names, update controller to include names on states

--- js/controller.js
+++ js/controller.js
@@ -4,10 +4,10 @@ stateMap.controller('interactiveMapCntrl',interactiveMapCntrl);
 
 stateMap.directive('clickState', function(){
 	return{
-		link: function($scope, element){
-
+		link: function($scope, element, attr){
 			element.bind('click', function(){
 				var newColor = getNewColor($scope.state);
+				$scope.state.stateColor = newColor;
 				var stateElement = element[0].querySelector('path');
 				stateElement.setAttribute('class','state ' + newColor);
 			});
@@ -16,18 +16,29 @@ stateMap.directive('clickState', function(){
 });
 
 function interactiveMapCntrl($scope){
+	console.log(redStates);
 	$scope.states = states;
+	for(i=0; i<redStates.length; i++){
+		$scope.redTotal = 
+	}
 }
 
 function getNewColor(state){
 	if(state.stateColor == 'red'){
+		//add the element to the appropriate array and remove it from the old
+        redStates[state.id] = ""
+		blueStates[state.id] = state;
 		return 'blue';
 	}else if(state.stateColor == 'blue'){
+        blueStates[state.id] = ""
+		openStates[state.id] = state;
 		return 'open';
 	}else if(state.stateColor == 'open'){
+        openStates[state.id] = ""
+		redStates[state.id] = state;
 		return "red";
 	}else{
-		return console.log("error");
+		return console.log("How did you get here?");
 	}
 	//if state color = red, then change to blue
 	//if state color = blue , then change to open
